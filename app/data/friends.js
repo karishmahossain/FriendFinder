58002a936bf144a49a74cf202ff3398459bd349e
Karishma <karishmah@gmail.com>
2019-09-03 22:04:06 -0700

    add files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Oprah",
+		photo: "http://static.oprah.com/images/o2/201407/201407-omag-WhatIKnowForSure-949x534.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Michelle Obama",
+		photo: "https://www.biography.com/.image/t_share/MTE5NDg0MDU0ODEyNzIyNzAz/michelle-obama-thumb-2.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Lana Del Rey",
+		photo: "https://images.complex.com/complex/images/c_limit,dpr_auto,q_90,w_720/fl_lossy,pg_1/kyrp3hyiw5jwvr9nuufn/lana",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Taylor Swift",
+		photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/taylor-swift-mtv-awards-1567449508.jpg?crop=0.665xw:1.00xh;0.206xw,0&resize=480:*",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Drake",
+		photo: "https://static01.nyt.com/images/2019/08/13/arts/12billboard/merlin_155953359_8b13fe87-faed-4a98-889d-c16d57735cf6-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
